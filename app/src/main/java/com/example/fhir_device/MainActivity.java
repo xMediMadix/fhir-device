a21d2e7f77b169e36d2864bb5bc0753a25940f75
Máté Madarász <medimadi@pop-os.localdomain>
2021-05-22 16:30:25 +0200

    Configure default theme, add menu bar with icons, create base home page

--- app/src/main/java/com/example/fhir_device/MainActivity.java
+++ app/src/main/java/com/example/fhir_device/MainActivity.java
@@ -1,13 +1,49 @@
 package com.example.fhir_device;
 
+import android.annotation.SuppressLint;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+    private static final String LOG_TAG = MainActivity.class.getName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        super.onCreateOptionsMenu(menu);
+        getMenuInflater().inflate(R.menu.nav_menu, menu);
+        return true;
+    }
+
+    @SuppressLint("NonConstantResourceId")
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.main:
+                Log.d(LOG_TAG, "Home page selected");
+                return true;
+            case R.id.listing:
+                Log.d(LOG_TAG, "Listing page selected");
+                return true;
+            case R.id.add:
+                Log.d(LOG_TAG, "Add page selected");
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    @Override
+    public boolean onPrepareOptionsMenu(Menu menu) {
+        return super.onPrepareOptionsMenu(menu);
     }
 }
